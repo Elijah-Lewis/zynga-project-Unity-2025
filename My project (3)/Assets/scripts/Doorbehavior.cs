a420b2607373355b86fc65644d338f6e79466016
tmclaughlinCS <tnmclaughlin@aggies.ncat.edu>
2025-04-03 16:54:33 -0400

    FIXED LOL

--- My project (3)/Assets/scripts/Doorbehavior.cs
+++ My project (3)/Assets/scripts/Doorbehavior.cs
@@ -18,7 +18,6 @@ public class DoorDeletion : MonoBehaviour
                 {
                     Destroy(door);
                     Debug.Log("Door deleted!");
-                    Application.LoadLevel(Application.loadedLevel);
                 }
                 else
                 {
