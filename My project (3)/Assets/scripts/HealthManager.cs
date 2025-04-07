c85c83872e1f598a5e72e0a169b74bfe4d14c64b
tmclaughlinCS <tnmclaughlin@aggies.ncat.edu>
2025-04-07 15:16:51 -0400

    added key model + hp bar aesthetics
    ammo ui next and skill tree

--- My project (3)/Assets/scripts/HealthManager.cs
+++ My project (3)/Assets/scripts/HealthManager.cs
@@ -5,52 +5,57 @@ public class HealthManager : MonoBehaviour
 {
     public Image healthBar;
     public float healthAmount = 100f;
+    public float maxHealth = 100f;
+    public float smoothSpeed = 5f; 
+    private float displayHealth; 
     public DungeonGenerator dungeonGenerator;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
     void Start()
     {
         dungeonGenerator = FindObjectOfType<DungeonGenerator>();
-        
+        displayHealth = healthAmount; 
     }
 
-    // Update is called once per frame
     void Update()
     {
         if (healthAmount <= 0)
         {
             ReloadLevel();
-
         }
-       if (Input.GetKeyDown(KeyCode.Return))
+
+        if (Input.GetKeyDown(KeyCode.Return))
         {
             TakeDamage(20);
         }
 
-       if(Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space))
         {
             Heal(5);
         }
+
+        
+        displayHealth = Mathf.Lerp(displayHealth, healthAmount, smoothSpeed * Time.deltaTime);
+
+        
+        healthBar.fillAmount = displayHealth / maxHealth;
     }
 
     public void TakeDamage(float damage)
     {
         healthAmount -= damage;
-        healthBar.fillAmount = healthAmount / 100f;
+        healthAmount = Mathf.Clamp(healthAmount, 0, maxHealth);
     }
 
     public void Heal(float healingAmount)
     {
         healthAmount += healingAmount;
-        healthAmount = Mathf.Clamp(healthAmount, 0, 100);
-
-        healthBar.fillAmount = healthAmount / 100f;
+        healthAmount = Mathf.Clamp(healthAmount, 0, maxHealth);
     }
 
     void ReloadLevel()
     {
         transform.position = dungeonGenerator.SpawnPosition;
-
-        healthAmount = 100f;
-        healthBar.fillAmount = healthAmount / 100f;
+        healthAmount = maxHealth;
+        displayHealth = maxHealth; 
     }
 }
